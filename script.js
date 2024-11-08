bd7212c8a6baaec207c4189d3d1f08145ad82df2
Naveen Kumar P <160722382+pknaveen-git@users.noreply.github.com>
2024-11-08 23:30:36 +0530

    testp1

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const noButton = document.getElementById("no-btn");
-const yesButton = document.getElementById("yes-btn");
-const popup = document.getElementById("popup");
+//const noButton = document.getElementById("no-btn");
+//const yesButton = document.getElementById("yes-btn");
+//const popup = document.getElementById("popup");
 
-noButton.addEventListener("mouseover", () => {
-  noButton.style.position = "absolute";
-  noButton.style.left = Math.random() * 80 + "vw";
-  noButton.style.top = Math.random() * 80 + "vh";
-});
+//noButton.addEventListener("mouseover", () => {
+  //noButton.style.position = "absolute";
+  //noButton.style.left = Math.random() * 80 + "vw";
+  //noButton.style.top = Math.random() * 80 + "vh";
+//});
 
-yesButton.addEventListener("click", () => {
+//yesButton.addEventListener("click", () => {
   // Play the sound when "Yes" is clicked
-  document.getElementById('yesSound').play();
+  //document.getElementById('yesSound').play();
   
   // Display the popup
-  popup.style.display = "block";
+  //popup.style.display = "block";
   
   // Hide the popup after 2 seconds
-  setTimeout(() => {
-    popup.style.display = "none";
-  }, 3000);
-});
+  //setTimeout(() => {
+    //popup.style.display = "none";
+  //}, 3000);
+//});
 
 // Hide the popup initially
-popup.style.display = "none";
+//popup.style.display = "none";
